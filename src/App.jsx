fce420396f6c7a7ec346d2b7f342b32bacb5a803
TaiDo <dotientai20031404@gmail.com>
2025-03-05 23:57:44 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-2xl">
+        Vite + React123
+      </h1>
+    </>
+  );
+}
+
+export default App;
